6c567c7eb9aa14da6a3a486b6ac3a58faa9c9a34
Pete Gadomski <pete.gadomski@gmail.com>
2023-09-27 08:48:44 -0400

    deps: ceil pydantic

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md") as f:
 install_requires = [
     "attrs",
     "orjson",
-    "pydantic[dotenv]>=1.10.8",  # https://github.com/pydantic/pydantic/issues/5821
+    "pydantic[dotenv]>=1.10.8,<2",  # https://github.com/pydantic/pydantic/issues/5821
     "stac_pydantic==2.0.*",
     "stac-fastapi.types~=2.4.8",
     "stac-fastapi.api~=2.4.8",
@@ -17,7 +17,7 @@ install_requires = [
     "buildpg",
     "brotli_asgi",
     "pygeofilter>=0.2",
-    "pypgstac>=0.7,<0.9",
+    "pypgstac~=0.8",
 ]
 
 extra_reqs = {
