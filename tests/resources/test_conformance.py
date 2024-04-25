5d8e8fece8790e9cf070dd268a514b9db772de2d
jonhealy1 <jonathan.d.healy@gmail.com>
2024-04-25 16:16:54 +0800

    update test

--- tests/resources/test_conformance.py
+++ tests/resources/test_conformance.py
@@ -56,9 +56,11 @@ async def test_landing_page_links(
     link_path = urllib.parse.urlsplit(link.get("href")).path
 
     # this feels hacky
-    assert link_path == (
-        app.state.router_prefix + app.state.router_prefix + expected_path
-    ) or (app.state.router_prefix + expected_path)
+    # assert link_path == (
+    #     app.state.router_prefix + app.state.router_prefix + expected_path
+    # ) or (app.state.router_prefix + expected_path)
+
+    assert link_path == app.state.router_prefix + expected_path
 
     resp = await app_client.get(link_path.rsplit("/", 1)[-1])
     assert resp.status_code == 200
