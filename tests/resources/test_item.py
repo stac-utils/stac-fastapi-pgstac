5f14e93e2199d74e7e929e4cb236c2b59cf89c53
vincentsarago <vincent.sarago@gmail.com>
2025-06-22 09:12:52 +0200

    update for stac-fastapi 6.0

--- tests/resources/test_item.py
+++ tests/resources/test_item.py
@@ -1356,52 +1356,6 @@ async def test_preserves_extra_link(
     assert extra_link[0]["href"] == expected_href
 
 
-async def test_item_search_post_filter_extension_cql_explicitlang(
-    app_client, load_test_data, load_test_collection
-):
-    """Test POST search with JSONB query (cql json filter extension)"""
-    test_item = load_test_data("test_item.json")
-    resp = await app_client.post(
-        f"/collections/{test_item['collection']}/items", json=test_item
-    )
-    assert resp.status_code == 201
-
-    # EPSG is a JSONB key
-    params = {
-        "collections": [test_item["collection"]],
-        "filter-lang": "cql-json",
-        "filter": {
-            "gt": [
-                {"property": "proj:epsg"},
-                test_item["properties"]["proj:epsg"] + 1,
-            ]
-        },
-    }
-    resp = await app_client.post("/search", json=params)
-    resp_json = resp.json()
-
-    assert resp.status_code == 200
-    assert len(resp_json.get("features")) == 0
-
-    params = {
-        "collections": [test_item["collection"]],
-        "filter-lang": "cql-json",
-        "filter": {
-            "eq": [
-                {"property": "proj:epsg"},
-                test_item["properties"]["proj:epsg"],
-            ]
-        },
-    }
-    resp = await app_client.post("/search", json=params)
-    resp_json = resp.json()
-    assert len(resp.json()["features"]) == 1
-    assert (
-        resp_json["features"][0]["properties"]["proj:epsg"]
-        == test_item["properties"]["proj:epsg"]
-    )
-
-
 async def test_item_search_post_filter_extension_cql2_2(
     app_client, load_test_data, load_test_collection
 ):
@@ -1622,26 +1576,6 @@ async def test_get_filter_extension(app_client, load_test_data, load_test_collec
     assert len(fc["features"]) == 1
     assert fc["features"][0]["id"] == search_id
 
-    # CQL-JSON
-    resp = await app_client.get(
-        "/search",
-        params={
-            "filter-lang": "cql-json",
-            "filter": json.dumps(
-                {
-                    "eq": [
-                        {"property": "id"},
-                        search_id,
-                    ],
-                },
-            ),
-        },
-    )
-    assert resp.status_code == 200
-    fc = resp.json()
-    assert len(fc["features"]) == 1
-    assert fc["features"][0]["id"] == search_id
-
     # CQL2-TEXT
     resp = await app_client.get(
         "/search",
@@ -1662,26 +1596,6 @@ async def test_get_filter_extension(app_client, load_test_data, load_test_collec
         params={
             "filter-lang": "cql2-json",
             "filter": json.dumps({"op": "in", "args": [{"property": "id"}, [search_id]]}),
-        },
-    )
-    assert resp.status_code == 200
-    fc = resp.json()
-    assert len(fc["features"]) == 1
-    assert fc["features"][0]["id"] == search_id
-
-    # CQL-JSON
-    resp = await app_client.get(
-        f"/collections/{collection_id}/items",
-        params={
-            "filter-lang": "cql-json",
-            "filter": json.dumps(
-                {
-                    "eq": [
-                        {"property": "id"},
-                        search_id,
-                    ],
-                },
-            ),
         },
     )
     assert resp.status_code == 200
