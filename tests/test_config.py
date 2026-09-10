74d2db5b13b8a79e40cd822e58bac8f102694bea
Vincent Sarago <vincent.sarago@gmail.com>
2026-09-10 14:15:52 +0200

    feat: remove deprecated POSTGRES_* settings (#420)

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,7 +3,6 @@
 import warnings
 
 import pytest
-from pydantic import ValidationError
 from pytest import MonkeyPatch
 
 from stac_fastapi.pgstac.config import PostgresSettings, Settings
@@ -19,19 +18,6 @@ async def test_pg_settings_with_env(monkeypatch):
     assert PostgresSettings(_env_file=None)
 
 
-async def test_pg_settings_with_env_postgres(monkeypatch):
-    """Test PostgresSettings with POSTGRES_* environment variables"""
-    monkeypatch.setenv("POSTGRES_USER", "username")
-    monkeypatch.setenv("POSTGRES_PASS", "password")
-    monkeypatch.setenv("POSTGRES_HOST_READER", "0.0.0.0")
-    monkeypatch.setenv("POSTGRES_HOST_WRITER", "0.0.0.0")
-    monkeypatch.setenv("POSTGRES_PORT", "1111")
-    monkeypatch.setenv("POSTGRES_DBNAME", "pgstac")
-    with pytest.warns(DeprecationWarning) as record:
-        assert PostgresSettings(_env_file=None)
-    assert len(record) == 6
-
-
 async def test_pg_settings_attributes(monkeypatch):
     """Test PostgresSettings with attributes"""
     with warnings.catch_warnings():
@@ -45,36 +31,6 @@ async def test_pg_settings_attributes(monkeypatch):
             _env_file=None,
         )
         assert settings.pghost == "0.0.0.0"
-
-    # Compat, should work with old style postgres_ attributes
-    # Should raise warnings on set attribute
-    with pytest.warns(DeprecationWarning) as record:
-        settings = PostgresSettings(
-            postgres_user="user",
-            postgres_pass="password",
-            postgres_host_reader="0.0.0.0",
-            postgres_port=1111,
-            postgres_dbname="pgstac",
-            _env_file=None,
-        )
-        assert settings.pghost == "0.0.0.0"
-        assert len(record) == 5
-
-    # Should raise warning when accessing deprecated attributes
-    with pytest.warns(DeprecationWarning):
-        assert settings.postgres_host_reader == "0.0.0.0"
-
-    with pytest.raises(ValidationError):
-        with pytest.warns(DeprecationWarning) as record:
-            PostgresSettings(
-                postgres_user="user",
-                postgres_pass="password",
-                postgres_host_reader="0.0.0.0",
-                postgres_host_writer="1.1.1.1",
-                postgres_port=1111,
-                postgres_dbname="pgstac",
-                _env_file=None,
-            )
 
 
 @pytest.mark.parametrize(
